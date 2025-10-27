a5416463842a43113d5d0ce3a5dad262d5d47223
Felix <felixhollndonner@hotmail.com>
2025-10-27 14:48:19 -0700

    Implemented security config.

--- apps/easybank-backend/src/main/java/at/holly/easybankbackend/controller/NoticesController.java
+++ apps/easybank-backend/src/main/java/at/holly/easybankbackend/controller/NoticesController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class NoticesController {
+public class NoticesController{
 
   @GetMapping("/notices")
   public String getNotices () {
