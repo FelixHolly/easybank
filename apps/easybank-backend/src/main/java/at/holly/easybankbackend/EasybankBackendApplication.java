63541e7cfecb24c15cfee57eea1f715ac8925a42
Felix <felixhollndonner@hotmail.com>
2025-10-27 14:20:04 -0700

    implemented controllers added scripts for backend init backend

---
+++ apps/easybank-backend/src/main/java/at/holly/easybankbackend/EasybankBackendApplication.java
@@ -0,0 +1,13 @@
+package at.holly.easybankbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasybankBackendApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(EasybankBackendApplication.class, args);
+    }
+
+}
---
+++ apps/easybank-backend/src/test/java/at/holly/easybankbackend/EasybankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package at.holly.easybankbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasybankBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
