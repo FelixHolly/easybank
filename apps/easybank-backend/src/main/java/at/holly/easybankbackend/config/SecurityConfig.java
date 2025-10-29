7ec17be2d3d32ec50f38290e343519acc7573485
Felix <felixhollndonner@hotmail.com>
2025-10-29 14:03:52 -0700

    added jpa dependencies disable form login & http basic

--- apps/easybank-backend/src/main/java/at/holly/easybankbackend/config/SecurityConfig.java
+++ apps/easybank-backend/src/main/java/at/holly/easybankbackend/config/SecurityConfig.java
@@ -3,9 +3,8 @@ package at.holly.easybankbackend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
@@ -17,8 +16,8 @@ public class SecurityConfig {
         .requestMatchers("/myAccount", "/myLoans", "/myCards", "/myBalance").authenticated()
         .requestMatchers("/contact", "/notices", "/error").permitAll()
     );
-    http.formLogin(withDefaults());
-    http.httpBasic(withDefaults());
+    http.formLogin(AbstractHttpConfigurer::disable);
+    http.httpBasic(AbstractHttpConfigurer::disable);
     return http.build();
   }
 
