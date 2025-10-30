74545d237c225ce4a82ad36193b4783975864fbf
Felix <felixhollndonner@hotmail.com>
2025-10-30 15:06:48 -0700

    added custom authorization exception (AccessDeniedHandler)

--- apps/easybank-backend/src/main/java/at/holly/easybankbackend/config/SecurityConfig.java
+++ apps/easybank-backend/src/main/java/at/holly/easybankbackend/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package at.holly.easybankbackend.config;
 
+import at.holly.easybankbackend.exceptionhandling.CustomAccessDeniedHandler;
 import at.holly.easybankbackend.exceptionhandling.CustomBasicAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -30,6 +31,7 @@ public class SecurityConfig {
       .exceptionHandling(
         (exceptions) -> exceptions
           .authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint())
+          .accessDeniedHandler(new CustomAccessDeniedHandler())
       )
       .formLogin(withDefaults())
       .httpBasic(hbc -> hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
---
+++ apps/easybank-backend/src/main/java/at/holly/easybankbackend/exceptionhandling/CustomAccessDeniedHandler.java
@@ -0,0 +1,32 @@
+package at.holly.easybankbackend.exceptionhandling;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
+import java.util.Date;
+
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+  @Override
+  public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+    Date date = new Date();
+    String errorMessage = (accessDeniedException != null && accessDeniedException.getMessage() != null) ? accessDeniedException.getMessage() : "Access denied";
+    String path = request.getRequestURI();
+
+    response.setHeader("easybank-error-reason", "Authorization denied");
+    response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+    response.setContentType("application/json;charset=utf-8");
+    response.getWriter().write("""
+        {
+        "status":"%s",
+        "path":"%s",
+        "timestamp": "%s",
+        "error": "%s"
+        }
+      """.formatted(HttpServletResponse.SC_FORBIDDEN, path, date, errorMessage)
+    );
+  }
+}
